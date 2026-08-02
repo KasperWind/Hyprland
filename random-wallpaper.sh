b3fb039fed6a3332258da8edcc9367faf65be074
Kasper Wind <kasperwind79@gmail.com>
2026-08-02 21:33:57 +0200

    ported to lua

--- random-wallpaper.sh
+++ random-wallpaper.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-directory=~/Pictures/Wallpapers
+directory=/home/kasperw/Pictures/Wallpapers
 monitor=`hyprctl monitors | rg Monitor | awk '{print $2}'`
 
 if [ -d "$directory" ]; then
 
     random_background=$(ls $directory/*.png | shuf -n 1)
 
-    echo $random_background
-
-    hyprctl hyprpaper unload all
-    hyprctl hyprpaper preload $random_background
     hyprctl hyprpaper wallpaper "$monitor, $random_background"
 
 fi
